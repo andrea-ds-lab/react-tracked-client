9880b8ebb10546c7dab64cb685ae0ffa80cb7926
andrea-ds-lab <andrea.deltasystem@gmail.com>
2024-08-01 15:55:08 +0200

    added rainbowkit wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { useWebSocket } from './sockets/websocketContext';
+import '@rainbow-me/rainbowkit/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider, http, useBlockNumber, useChainId, useChains, useClient } from 'wagmi';
+import { base, mainnet, polygon } from 'wagmi/chains';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: '482686d94bcc46ed35e83e07b3996bbf', // Replace with your actual project ID
+  chains: [mainnet, polygon, base],
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 
 function App() {
   const [count, setCount] = useState(0);
   const { channel } = useWebSocket();
+
+  const { ...chains } = useChains()
+  const chainId = useChainId()
+  const blockNumber = useBlockNumber()
+
+  useEffect(() => {
+    console.log(chains)
+    console.log(chainId)
+    console.log("Block number: %d", blockNumber)
+  }, [chainId])
 
   const logClick = () => {
     setCount(count + 1);
@@ -29,9 +57,22 @@ function App() {
         <button onClick={logClick}>
           Messaggi inviati al server: {count}
         </button>
+        <ConnectButton />
       </div>
     </>
   );
 }
 
-export default App;
+function WrappedApp() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default WrappedApp;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import "./assets/css/theme.css"
 import { WebSocketProvider } from './sockets/websocketContext.tsx'
+import WrappedApp from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WebSocketProvider>
-      <App />
+      < WrappedApp />
     </WebSocketProvider>
   </React.StrictMode>,
 )
